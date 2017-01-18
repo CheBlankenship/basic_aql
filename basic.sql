ae072b9b7bc41f1f0c1458db3505e42986b2b1ce
Che B <cnakamura336@gmail.com>
2017-01-17 19:55:26 -0500

    basic querying

---
+++ basic.sql
@@ -0,0 +1,10 @@
+-- 1,Find the title of each film ✓
+-- 2,Find the director of each film
+-- 3,Find the title and director of each film
+-- 4,Find the title and year of each film
+-- 5, Find all the information about each film
+
+1, SELECT title FROM movies;
+2, SELECT director FROM movies;
+3, SELECT title, director FROM movies;
+4, SELECT title, year FROM movies;
